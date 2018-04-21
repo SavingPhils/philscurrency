12ce1a24cb6c5e31319822e3ca2d127dce2bdb44
Zvi ben Yaakov <zvi.io.github@infosoc.net>
2018-04-21 23:01:58 +0300

    Check against block pos instead of index height

--- src/main.cpp
+++ src/main.cpp
@@ -1980,7 +1980,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
 {
     AssertLockHeld(cs_main);
     // Check it again in case a previous version let a bad block in
-    if (!CheckBlock(block, state, !fJustCheck, !fJustCheck) || pindex->nHeight => FORK_BLOCK)
+    if (!CheckBlock(block, state, !fJustCheck, !fJustCheck) || pindex->GetBlockPos() => FORK_BLOCK)
         return false;
 
     // verify that the view's current state corresponds to the previous block
